8c2179d578ddd791e91e62b7100cd3a44425aa7a
Viveks610 <vivek.shrirame123@gmail.com>
2023-06-16 15:13:28 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Gradient from "./components/Gradient";
+
+function App() {
+  return <div className="app">
+  <Gradient/>
+  </div>;
+}
+
+export default App;
---
+++ src/components/Gradient.tsx
@@ -0,0 +1,12 @@
+const Gradient = () => {
+  return (
+    <div className="app">
+      <h1>Create your own gradient!</h1>
+      <div className="colorPicker">
+        <input type="color" />
+      </div>
+    </div>
+  );
+};
+
+export default Gradient;
